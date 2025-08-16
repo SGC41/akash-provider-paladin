37ae00643791d499128259f9c6340dc4fc46d9f1
SGC41 <104068405+SGC41@users.noreply.github.com>
2025-08-17 00:05:59 +0200

    Update edit-yaml.sh

--- edit-yaml.sh
+++ edit-yaml.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
 cd $HOME/akash-provider-paladin
-update-local-provider-yaml.sh && \
-nano provider.yaml && \
-update-cluster-provider-yaml.sh && \
+$HOME/akash-provider-paladin/update-local-provider-yaml.sh && \
+nano $HOME/akash-provider-paladin/provider.yaml && \
+$HOME/akash-provider-paladin/update-cluster-provider-yaml.sh && \
 
-read -p "Do you want to continue? [Y/n] " choice
+read -p "Do you want to continue? [Y/n] ?" choice
 choice=${choice:-Y}  # Default to Y if empty
 
 case "$choice" in
   [Yy]* )
     echo "Restarting provider using update-provider-configuration.sh"
-    update-provider-configuration.sh
+    $HOME/akash-provider-paladin/update-provider-configuration.sh
     ;;
   [Nn]* )
     echo "Aborted."
