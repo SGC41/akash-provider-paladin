489f6d8f48bfead6d878e831ec8476ac70e9c356
SGC41 <104068405+SGC41@users.noreply.github.com>
2025-09-24 23:59:29 +0000

    Paladin v2.9.0

--- install.sh
+++ install.sh
@@ -190,7 +190,7 @@ mkdir -p /var/log/akash-provider-paladin
 chown root:root /var/log/akash-provider-paladin
 chmod 755 /var/log/akash-provider-paladin
 
-CRONLINE="*/1 * * * * [ -f /tmp/control-plane.do ] && /bin/bash \"/root/akash-provider-paladin/scripts/ticker-control-plane.sh\" >> \"/var/log/akash-provider-paladin/\$(date +\\%d)-this-month.log\" 2>&1 && rm -f /tmp/control-plane.do"
+CRONLINE="*/2 * * * * [ -f /tmp/control-plane.do ] && /bin/bash \"/root/akash-provider-paladin/scripts/ticker-control-plane.sh\" >> \"/var/log/akash-provider-paladin/\$(date +\\%d)-this-month.log\" 2>&1 && rm -f /tmp/control-plane.do"
 
 #CRONLINE="*/1 * * * * [ -f /tmp/control-plane.do ] && /bin/bash "/root/akash-provider-paladin/scripts/ticker-control-plane.sh" >> "/var/log/akash-provider-paladin/$(date +\%d)-this-month.log" 2>&1 && rm -f /tmp/control-plane.do"
 SCRIPT_PATH="akash-provider-paladin"
