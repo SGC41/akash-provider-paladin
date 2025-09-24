64eecd1791cfcee32f1c15b447bb596b00a25d81
SGC41 <104068405+SGC41@users.noreply.github.com>
2025-09-24 18:27:12 +0000

    Paladin v2.9.0

--- scripts/ticker.sh
+++ scripts/ticker.sh
@@ -166,27 +166,9 @@ if kubectl -n "$NS" get pod "$POD" &>/dev/null; then
 
           # Check if RPC node is working
           echo "Verifying RPC node is working"
-          RAW=$(kubectl exec -n akash-services akash-provider-0 -c provider -- \
-            printenv AKASH_NODE_1_PORT_26657_TCP 2>/dev/null)
 
-          if [[ -z "$RAW" ]]; then
-            echo "No AKASH_NODE_1_PORT_26657_TCP found"
-            exit 1
-          fi
-
-          HOST=$(echo "$RAW" | sed -E 's#^tcp://([^:]+):([0-9]+)$#\1#')
-          PORT=$(echo "$RAW" | sed -E 's#^tcp://([^:]+):([0-9]+)$#\2#')
-
-          if [[ "$PORT" == "26657" ]]; then
-            SCHEME="http"
-          elif [[ "$PORT" == "440" ]]; then
-            SCHEME="https"
-          else
-            SCHEME="https"
-          fi
-
-          probe_url="${SCHEME}://${HOST}:${PORT}"
-          echo "$probe_url"
+          probe_url=$(< /host/tmp/akash-provider-rpc.conf)
+          echo "loaded rpc node from file /tmp/akash-provider-rpc.conf $probe_url"
 
           hrs=$(check_rpc "$probe_url") || {
             echo "[rpc] $probe_url failed health check — sending RPC rotate"
